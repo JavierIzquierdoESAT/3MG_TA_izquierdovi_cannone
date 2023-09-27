63432e227639de36ba9ee1af3ce6f20ad450d942
izquierdovi <izquierdovi@esat-libertad.com>
2023-09-27 19:47:53 +0200

    creating and deleting window

--- include/window.hpp
+++ include/window.hpp
@@ -6,17 +6,23 @@
 class Window
 {
 public:
-	static std::optional<Window> Make(int w, int h, const char* title);
+
 	~Window();
-	Window(Window&& w);
+	Window(const Window&) = delete;
+	//lvalue move constructor
+	Window(Window& w);
+	//rvalue move constructor
+	Window(Window&& w) noexcept;
 
+	static std::optional<Window> Make(int w, int h, const char* title);
+
+	/// <summary>
+	/// checks if the should be closed and refreshes buffer
+	/// </summary>
 	bool isDone() const;
-
 
 private:
 	Window(int w, int h, const char* title);
 
-	
-
-	struct GLFWwindow* window;
+	struct GLFWwindow* window_handle_;
 };
--- src/window.cpp
+++ src/window.cpp
@@ -1,33 +1,38 @@
-
 #include "window.hpp"
 #include <GLFW/glfw3.h>
 
-
-Window::Window(int w, int h, const char* title)
+Window::Window(int w, int h, const char* title):
+	window_handle_{nullptr}
 {
   glfwInit();
-  window = glfwCreateWindow(w, h, title, NULL, NULL);
+  window_handle_ = glfwCreateWindow(w, h, title, NULL, NULL);
 }
 
-Window::Window(Window&& w)
+Window::Window(Window& w) : window_handle_{ w.window_handle_ }
 {
-  this->window = w.window;
-  w.window = NULL;
+  w.window_handle_ = NULL;
+}
+
+Window::Window(Window&& w) noexcept : window_handle_{w.window_handle_}
+{
+  w.window_handle_ = NULL;
+}
+
+Window::~Window()
+{
+  if (window_handle_!=NULL) {
+    glfwDestroyWindow(window_handle_);
+    glfwTerminate();
+  }
+
 }
 
 std::optional<Window> Window::Make(int w, int h, const char* title)
 {
-  return Window(w, h, title);
-}
-
-
-
-Window::~Window()
-{
-  glfwDestroyWindow(window);
-  glfwTerminate();
+  return Window{ w, h, title };
 }
 
 bool Window::isDone() const{
-  return glfwWindowShouldClose(window);
+  glfwPollEvents();
+  return glfwWindowShouldClose(window_handle_);
 }
