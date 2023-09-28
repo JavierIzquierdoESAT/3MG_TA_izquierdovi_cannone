ad8eebadeb8299d780ab83c79a572c982db156b4
Javier Izquierdo <izquierdovi@esat-alumni.com>
2023-09-28 10:11:11 +0200

    windows properly following style guide

--- include/window.hpp
+++ include/window.hpp
@@ -1,27 +1,29 @@
 #include "../src/stdafx.hpp"
 
-
+/** Handles Window */
 class Window
 {
  public:
-
-	~Window();
+	//copy constructor
 	Window(const Window&) = delete;
 	//lvalue move constructor
 	Window(Window& w);
 	//rvalue move constructor
 	Window(Window&& w) noexcept;
 
+	~Window();
+
+	/** creates the window */
 	static std::optional<Window> Make(int w, int h, const char* title);
 
-	/// <summary>
-	/// checks if the should be closed and refreshes buffer
-	/// </summary>
+	/** checks if the should be closed and refreshes buffer */
 	bool isDone() const;
 
  private:
+
 	Window(int w, int h, const char* title);
 
+	/** openGL window handle */
 	struct GLFWwindow* window_handle_;
 
 };
--- src/window.cpp
+++ src/window.cpp
@@ -1,39 +1,34 @@
 #include "window.hpp"
+
 #include <GLFW/glfw3.h>
 
-Window::Window(int w, int h, const char* title):
-	window_handle_{nullptr}
-{
+Window::Window(int w, int h, const char* title)
+    : window_handle_{nullptr} {
   glfwInit();
   window_handle_ = glfwCreateWindow(w, h, title, NULL, NULL);
 }
 
-Window::Window(Window& w) : window_handle_{ w.window_handle_ }
-{
+Window::Window(Window& w) : window_handle_{w.window_handle_} {
   w.window_handle_ = NULL;
 }
 
 Window::Window(Window&& w) noexcept 
-  : window_handle_{w.window_handle_}
-{
+    : window_handle_{w.window_handle_} {
   w.window_handle_ = NULL;
 }
 
-Window::~Window()
-{
-  if (window_handle_!=NULL) {
+Window::~Window() {
+  if (NULL != window_handle_) {
     glfwDestroyWindow(window_handle_);
     glfwTerminate();
   }
-
 }
 
-std::optional<Window> Window::Make(int w, int h, const char* title)
-{
+std::optional<Window> Window::Make(int w, int h, const char* title) {
   return Window{ w, h, title };
 }
 
-bool Window::isDone() const{
+bool Window::isDone() const {
   glfwPollEvents();
   return glfwWindowShouldClose(window_handle_);
 }
