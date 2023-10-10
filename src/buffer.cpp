6edf74f415dad15fbd3f83dfa3380c842c192992
SimoneCannone <cannone@esat-alumni.com>
2023-10-10 10:48:28 +0200

    fix buffer update

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -6,12 +6,14 @@ Buffer::Buffer(unsigned int size) : size_{size} {
   glGenBuffers(1, &buffer_id_);
   bindBuffer(kTarget_Vertex_Data);
   glBufferData(GL_ARRAY_BUFFER, size, NULL, GL_STATIC_DRAW);
+  glGenVertexArrays(1, &vertex_array_id_);
 }
 
 Buffer::Buffer(const void* data, unsigned int size) : size_{size} {
   glGenBuffers(1, &buffer_id_);
   bindBuffer(kTarget_Vertex_Data);
-  glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, size, data, GL_DYNAMIC_DRAW);
+  glGenVertexArrays(1, &vertex_array_id_);
 }
 
 Buffer::~Buffer() {
@@ -46,20 +48,10 @@ void Buffer::uploadData(const void* data, unsigned int size,
   glBufferSubData(GL_ARRAY_BUFFER, offset, size, data);
 }
 
-void Buffer::updateData(const void* data, unsigned int size) {
-  
-  bindBuffer(kTarget_Vertex_Data);
-  glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 12, size, data);
-  
-}
-
 void Buffer::reset(const void* data, unsigned int size) {
   glDeleteBuffers(1, &buffer_id_);
   glDeleteVertexArrays(1, &vertex_array_id_);
 
-  //glGenBuffers(1, &buffer_id_);
-  //bindBuffer(kTarget_Vertex_Data);
-  //glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
 }
 
 void Buffer::enableVertexArray(const unsigned int size,
@@ -67,8 +59,7 @@ void Buffer::enableVertexArray(const unsigned int size,
                                const unsigned int offset) {
   // TODO:: UNHARCODE this part. Now is set to print a triangle vertex with
   // color
-  glGenVertexArrays(1, &vertex_array_id_);
-
+  
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
 
@@ -76,6 +67,8 @@ void Buffer::enableVertexArray(const unsigned int size,
   glEnableVertexAttribArray(1);
 
   bindVertexArray();
+
+ 
 }
 
 unsigned Buffer::buffer_id() const { return buffer_id_; }
