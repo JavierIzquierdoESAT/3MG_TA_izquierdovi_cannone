fb40a47e1c2644b2deefb6859ecbb646943552c2
SimoneCannone <cannone@esat-alumni.com>
2023-10-11 11:20:26 +0200

    prepare premake

--- premake5.lua
+++ premake5.lua
@@ -122,3 +122,18 @@ project"Window"
     debugargs { _MAIN_SCRIPT_DIR .. "/examples/data" }
 
     files "examples/window.cpp"
+
+  project"MultyThread"
+
+      kind "ConsoleApp"
+      language "C++"
+      targetdir "build/%{prj.name}/%{cfg.buildcfg}"
+      includedirs "include"
+      links "Motor"
+
+      conan_config_exec("Debug")
+      conan_config_exec("Release")
+      conan_config_exec("RelWithDebInfo")
+      debugargs { _MAIN_SCRIPT_DIR .. "/examples/data" }
+
+      files "examples/multythread_test.cpp"
