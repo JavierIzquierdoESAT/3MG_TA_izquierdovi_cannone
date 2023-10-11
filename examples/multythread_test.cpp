82a7f230ed1790921645b93fc770a6e5b0ad0ec5
cannone <cannone@esat-alumni.com>
2023-10-11 20:17:35 +0200

    first try multythread

--- examples/multythread_test.cpp
+++ examples/multythread_test.cpp
@@ -3,26 +3,24 @@
 #include <iostream>
 #include <queue>
 
-std::string getString(std::string a) {
-   return a;
-}
+std::string getString(std::string a) { 
+    
+    std::cout << a << std::endl;
+    return a; }
 
-class Texture {};
-
-Texture load_texture(std::string texture);
 
 class JobSystem {
  public:
   // template<typename Function>
   // auto add_complicaete(Function task()) -> std::future<decltype(Function())>
   // { 	using Ret = decltype(Function()); 	auto t =
-  //std::packaged_task<Ret()>(task); 	std::future<Ret> future = t.get_future();
-  //	jobs_.push(t);
+  // std::packaged_task<Ret()>(task); 	std::future<Ret> future =
+  // t.get_future(); 	jobs_.push(t);
 
   //	return future;
   //}
 
-  JobSystem() : stop_{false}{
+  JobSystem() : stop_{true} {
     auto worker_count = std::thread::hardware_concurrency();
     for (int i = 0; i != worker_count; i++) {
       workers_.emplace_back(std::thread(&JobSystem::work, this));
@@ -43,9 +41,22 @@ class JobSystem {
       task();
     }
   }
-  void set_stop(bool t) {stop_ = t;}
 
-  void add(std::function<void()> task) { jobs_.push(std::move(task)); }
+  ~JobSystem() {
+    for (auto& t : workers_) {
+      if (t.joinable()) {
+        t.join();
+      }
+
+    }
+    }
+
+  void set_stop(bool t) { stop_ = t; }
+
+  void add(std::function<void()> task) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    jobs_.push(std::move(task));
+  }
 
  private:
   bool stop_;
@@ -56,24 +67,20 @@ class JobSystem {
 };
 
 int main(int, char**) {
+
   JobSystem b;
 
-  std::vector<std::string> comida{"patata.jpg", "melon.jpg", "zanahoria.jpg",
-                                  "pimiento.jpg"};
+  std::vector<std::string> comida{"patata.jpg\n", "melon.jpg\n", "zanahoria.jpg\n", "pimiento.jpg\n", "javi.jpg",};
   std::vector<std::future<std::string>> resultado;
 
   for (auto& ente : comida) {
     auto mycall = [ente]() { return getString(ente); };
-    auto task = std::packaged_task<std::string()>(std::move(mycall));
-    std::future<std::string> future = task.get_future();
-    b.add(std::move(task));
-    //resultado.emplace_back(future);
+    std::shared_ptr<std::packaged_task<std::string()>> task = std::make_shared<std::packaged_task<std::string()>>(std::move(mycall));
+    //auto task = std::packaged_task<std::string()>(std::move(mycall));
+    std::future<std::string> future = task->get_future();
+    b.add([task]() { (*task)(); });
+    resultado.push_back(std::move(future));
   }
-
-  struct lamba {
-     auto ente;
-    std::string lambda::operator()(ente) {}
-  };
 
   return 0;
 }
