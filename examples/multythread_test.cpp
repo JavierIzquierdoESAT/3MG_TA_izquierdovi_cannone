840142568c7adcaf461b7deb475c28fd4065a951
izquierdovi <izquierdovi@esat-alumni.com>
2023-10-17 20:22:16 +0200

    tested and working

--- examples/multythread_test.cpp
+++ examples/multythread_test.cpp
@@ -1,7 +1,11 @@
 #include <functional>
 #include <future>
 #include <iostream>
+#include <optional>
 #include <queue>
+
+#include <chrono>
+#include <thread>
 
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
@@ -16,9 +20,19 @@ std::string getString(std::string a) {
 
 class Texture {
  public:
-  static Texture LoadTexture(const char* name) {
+  Texture(std::string t)
+      : texture_{0} {
+
+        };
+
+  Texture() {
+    glGenTextures(1, &texture_);
+    glBindTexture(GL_TEXTURE_2D, texture_);
+  }
+
+  static Texture LoadTexture(const char* a) {
+    // return TTest(a);
     Texture t = Texture();
-    std::cout << name << ": Start" << std::endl;
 
     // set the texture wrapping/filtering options (on the currently bound
     // texture object)
@@ -29,7 +43,7 @@ class Texture {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load and generate the texture
     int width, height, nrChannels;
-    unsigned char* data = stbi_load(name, &width, &height, &nrChannels, 0);
+    unsigned char* data = stbi_load(a, &width, &height, &nrChannels, 0);
     if (data) {
       glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
                    GL_UNSIGNED_BYTE, data);
@@ -37,19 +51,13 @@ class Texture {
       std::cout << "Failed to load texture" << std::endl;
     }
     stbi_image_free(data);
-    std::cout << name << ": Loaded" << std::endl;
-  
+    std::cout << a << ": Loaded" << std::endl;
+
     return t;
   }
-
-  ~Texture() = default;
+  unsigned int texture_;
 
  private:
-  Texture() {
-    glGenTextures(1, &texture_);
-    glBindTexture(GL_TEXTURE_2D, texture_);
-  }
-  unsigned int texture_;
 };
 
 class JobSystem {
@@ -63,11 +71,12 @@ class JobSystem {
   //	return future;
   //}
 
-  JobSystem() : stop_{true} {
+  JobSystem() : stop_{false} {
     auto worker_count = std::thread::hardware_concurrency();
     for (int i = 0; i != worker_count; i++) {
       workers_.emplace_back(std::thread(&JobSystem::work, this));
     }
+    std::cout << worker_count << std::endl;
   }
 
   void work() {
@@ -75,7 +84,10 @@ class JobSystem {
       std::function<void()> task;
       {
         std::unique_lock<std::mutex> lock(mutex_);
-        condition_.wait(lock, [this] { return !jobs_.empty() || stop_; });
+        condition_.wait(lock, [this] {
+          return !jobs_.empty() || stop_;
+        });
+
         if (stop_ && jobs_.empty()) return;
         task = std::move(jobs_.front());
         jobs_.pop();
@@ -86,6 +98,7 @@ class JobSystem {
   }
 
   ~JobSystem() {
+    condition_.notify_all();
     for (auto& t : workers_) {
       if (t.joinable()) {
         t.join();
@@ -98,6 +111,7 @@ class JobSystem {
   void add(std::function<void()> task) {
     std::lock_guard<std::mutex> lock(mutex_);
     jobs_.push(std::move(task));
+    condition_.notify_one();
   }
 
  private:
@@ -109,10 +123,15 @@ class JobSystem {
 };
 
 int main(int, char**) {
-  JobSystem b;
   glfwInit();
-  std::vector<std::string> comida{"../assets/4k.png"};
-  std::vector<std::future<Texture>> resultado;
+  JobSystem b;
+
+  std::vector<std::string> comida{
+      "../assets/4k.png", "../assets/4k.png",
+      "../assets/4k.png", "../assets/4k.png",
+      "../assets/4k.png", "../assets/4k.png"
+  };
+  std::vector<std::future<std::optional<Texture>>> resultado;
 
   double start = glfwGetTime();
   for (auto& ente : comida) {
@@ -120,28 +139,85 @@ int main(int, char**) {
   }
   std::cout << glfwGetTime() - start << std::endl;
 
+
   start = glfwGetTime();
   for (auto& ente : comida) {
     auto mycall = [ente]() { return Texture::LoadTexture(ente.c_str()); };
-
-    std::shared_ptr<std::packaged_task<Texture()>> task =
-        std::make_shared<std::packaged_task<Texture()>>(std::move(mycall));
-    std::future<Texture> future = task->get_future();
+    std::shared_ptr<std::packaged_task<std::optional<Texture>()>> task =
+        std::make_shared<std::packaged_task<std::optional<Texture>()>>(
+            std::move(mycall));
+    // auto task = std::packaged_task<std::string()>(std::move(mycall));
+    std::future<std::optional<Texture>> future = task->get_future();
     b.add([task]() { (*task)(); });
     resultado.push_back(std::move(future));
   }
 
   int count = 0;
+
   while (true) {
     for (auto& a : resultado) {
       if (a.wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
         count++;
       }
     }
-    if (count == resultado.size() - 1) break;
+    if (count == resultado.size()) break;
     count = 0;
   }
+
   std::cout << glfwGetTime() - start << std::endl;
+
+  for (auto& a : resultado) {
+    std::cout << a.get()->texture_;
+  }
+
+
+
 
   return 0;
 }
+
+// int main(int, char**) {
+//  JobSystem b;
+//  glfwInit();
+//  std::vector<std::string> comida{"../assets/4k.png"};
+//  std::vector<std::future<std::optional<std::string>>> resultado;
+//
+//  double start = glfwGetTime();
+//  for (auto& ente : comida) {
+//    // Texture t = Texture::LoadTexture(ente.c_str());
+//  }
+//  // std::cout << glfwGetTime() - start << std::endl;
+//
+//  start = glfwGetTime();
+//  for (auto& ente : comida) {
+//    auto mycall = [ente]() { return TTest::getString(ente); };
+//
+//    std::shared_ptr<std::packaged_task<std::optional<std::string>()>> task;
+//    task = std::make_shared<std::packaged_task<std::optional<std::string>()>>(
+//        std::move(mycall));
+//
+//    std::future<std::optional<std::string>> future = task->get_future();
+//    b.add([task]() { (*task)(); });
+//    resultado.push_back(std::move(future));
+//  }
+//
+//  while (resultado[0].wait_for(std::chrono::seconds(0)) !=
+//         std::future_status::ready) {
+//  }
+//  std::cout << glfwGetTime() - start << std::endl;
+//
+//  // int count = 0;
+//
+//  // while (true) {
+//  //  for (auto& a : resultado) {
+//  //    if (a.wait_for(std::chrono::seconds(0)) == std::future_status::ready)
+//  {
+//  //      count++;
+//  //    }
+//  //  }
+//  //  if (count == resultado.size()) break;
+//  //  count = 0;
+//  //}
+//
+//  return 0;
+//}
