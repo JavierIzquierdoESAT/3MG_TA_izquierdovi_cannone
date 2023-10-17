90d43fff7750d2a80427ec53b883f27ef8d0d385
Javier Izquierdo <izquierdovi@esat-alumni.com>
2023-10-17 13:25:21 +0200

    needs to call default constructor but I dont want a default constructor

--- examples/multythread_test.cpp
+++ examples/multythread_test.cpp
@@ -3,11 +3,54 @@
 #include <iostream>
 #include <queue>
 
-std::string getString(std::string a) { 
-    
-    std::cout << a << std::endl;
-    return a; }
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+std::string getString(std::string a) {
+  std::cout << a << std::endl;
+  return a;
+}
+
+class Texture {
+ public:
+  static Texture LoadTexture(const char* name) {
+    Texture t = Texture();
+    std::cout << name << ": Start" << std::endl;
+
+    // set the texture wrapping/filtering options (on the currently bound
+    // texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                    GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load and generate the texture
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load(name, &width, &height, &nrChannels, 0);
+    if (data) {
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                   GL_UNSIGNED_BYTE, data);
+    } else {
+      std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+    std::cout << name << ": Loaded" << std::endl;
+  
+    return t;
+  }
+
+  ~Texture() = default;
+
+ private:
+  Texture() {
+    glGenTextures(1, &texture_);
+    glBindTexture(GL_TEXTURE_2D, texture_);
+  }
+  unsigned int texture_;
+};
 
 class JobSystem {
  public:
@@ -47,9 +90,8 @@ class JobSystem {
       if (t.joinable()) {
         t.join();
       }
-
     }
-    }
+  }
 
   void set_stop(bool t) { stop_ = t; }
 
@@ -67,20 +109,39 @@ class JobSystem {
 };
 
 int main(int, char**) {
-
   JobSystem b;
+  glfwInit();
+  std::vector<std::string> comida{"../assets/4k.png"};
+  std::vector<std::future<Texture>> resultado;
 
-  std::vector<std::string> comida{"patata.jpg\n", "melon.jpg\n", "zanahoria.jpg\n", "pimiento.jpg\n", "javi.jpg",};
-  std::vector<std::future<std::string>> resultado;
-
+  double start = glfwGetTime();
   for (auto& ente : comida) {
-    auto mycall = [ente]() { return getString(ente); };
-    std::shared_ptr<std::packaged_task<std::string()>> task = std::make_shared<std::packaged_task<std::string()>>(std::move(mycall));
-    //auto task = std::packaged_task<std::string()>(std::move(mycall));
-    std::future<std::string> future = task->get_future();
+    Texture t = Texture::LoadTexture(ente.c_str());
+  }
+  std::cout << glfwGetTime() - start << std::endl;
+
+  start = glfwGetTime();
+  for (auto& ente : comida) {
+    auto mycall = [ente]() { return Texture::LoadTexture(ente.c_str()); };
+
+    std::shared_ptr<std::packaged_task<Texture()>> task =
+        std::make_shared<std::packaged_task<Texture()>>(std::move(mycall));
+    std::future<Texture> future = task->get_future();
     b.add([task]() { (*task)(); });
     resultado.push_back(std::move(future));
   }
+
+  int count = 0;
+  while (true) {
+    for (auto& a : resultado) {
+      if (a.wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
+        count++;
+      }
+    }
+    if (count == resultado.size() - 1) break;
+    count = 0;
+  }
+  std::cout << glfwGetTime() - start << std::endl;
 
   return 0;
 }
