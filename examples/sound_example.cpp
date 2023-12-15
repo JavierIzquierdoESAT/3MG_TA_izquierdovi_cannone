917a7e61c993c2a2bcc67b9e0321be389f1aba11
SimoneCannone <cannone@esat-alumni.com>
2023-12-15 15:13:31 +0100

    add coments

--- examples/sound_example.cpp
+++ examples/sound_example.cpp
@@ -18,15 +18,18 @@ int main(int argv, char** args) {
     Window window = Window::Make(engine, 640, 480, "ventana");
 
     ComponentManager component_manager;
-
+    //add the new type of component to the component manager
     component_manager.addComponentClass<AudioSource>(ComponentListType::kCompact);
 
     ALfloat a[] = { 0,0,0 };
     ALfloat b[] = { 0,0,0 };
+
+    //Create the components of audio
     AudioSource audio("Other", a, b, 1.0f);
     AudioSource audio2("Other 2", a, b, 1.0f);
     AudioSource audio3("Vocals", a, b, 1.0f);
 
+    //Inizilize sound buffers
     SoundBuffer buf =
         SoundBuffer::MakeBuffer("../assets/Invaders-CivilWar/other.wav")
             .value();
@@ -34,11 +37,13 @@ int main(int argv, char** args) {
         SoundBuffer::MakeBuffer("../assets/Invaders-CivilWar/vocals.wav")
             .value();
 
+    //Add buffers to the sources
     audio.src.addSound(&buf);
     audio2.src.addSound(&buf);
     audio3.src.addSound(&buf2);
 
     Position p(0.0f, 0.0f, 0.0f);
+    //set the status of the sources
     audio.src.start_ = false;
     audio.src.stop_ = false;
 
@@ -48,6 +53,7 @@ int main(int argv, char** args) {
     audio3.src.start_ = false;
     audio3.src.stop_ = false;
 
+    //Create the enyitys with than components
     component_manager.addEntity<Position, AudioSource>(p, audio);
     component_manager.addEntity<Position, AudioSource>(p, audio2);
     component_manager.addEntity<Position, AudioSource>(p, audio3);
@@ -57,11 +63,10 @@ int main(int argv, char** args) {
 
     while (!window.isDone()) {
 
-        // Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
+        
         glClear(GL_COLOR_BUFFER_BIT);
         
-
-
+        //Use the system to set the status od the audio comps
         SoundSystem(component_manager.getCompactIterator<AudioSource>(),
                     component_manager.getIterator<Position>());
 
@@ -70,7 +75,7 @@ int main(int argv, char** args) {
         window.update();
     }
 
-
+    //unbid all the buffers from the sources before delete
     UnbindSoundSystem(component_manager.getCompactIterator<AudioSource>());
 
     return 0;
